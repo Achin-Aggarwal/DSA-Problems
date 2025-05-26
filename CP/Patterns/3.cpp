4684bf3e2b143f6bdb6339e44c4570d139adf7c0
AchinAggarwal <22f1001390@ds.study.iitm.ac.in>
2025-05-26 18:21:12 +0000

    Patterns Updated

--- CP/Patterns/3.cpp
+++ CP/Patterns/3.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++){
-            cout<<"* ";
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
         }
         cout<<endl;
     }
 }
-
 
 
 // Input :- 
@@ -22,8 +21,8 @@ int main(){
 
 // output :- 
 
-// * 
-// * * 
-// * * * 
-// * * * * 
-// * * * * * 
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
